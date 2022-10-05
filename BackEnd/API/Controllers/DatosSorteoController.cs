9e79bb8bd99b117d96f8a4814a1932eb9a93523c
marcelo.caracas.leiva <51307856+mcaracas@users.noreply.github.com>
2022-10-05 10:22:41 -0600

    HttpDelete

--- BackEnd/API/Controllers/DatosSorteoController.cs
+++ BackEnd/API/Controllers/DatosSorteoController.cs
@@ -49,4 +49,15 @@ public class DatosSorteoController : ControllerBase
         DatosSorteoUpdate.IdInterno = datosSorteo.IdInterno;
         context.SaveChanges();
     }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        var context = new proyecto_bdContext();
+        var datosSorteo = context.DatosSorteos.FirstOrDefault(x => x.IdInterno == id);
+        context.DatosSorteos.Remove(datosSorteo);
+        context.SaveChanges();
+    }
+
+    
 }
