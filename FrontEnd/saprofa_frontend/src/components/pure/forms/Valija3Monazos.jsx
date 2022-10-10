5f5304ce16beb34132b406ea2b5f1bd7e5ad9e3d
Gianluca Gibellato <gnlgibe@gmail.com>
2022-10-10 09:41:29 -0600

    Adding format and classNames to valija forms

--- FrontEnd/saprofa_frontend/src/components/pure/forms/Valija3Monazos.jsx
+++ FrontEnd/saprofa_frontend/src/components/pure/forms/Valija3Monazos.jsx
@@ -1,33 +1,154 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+
+const pruebasSchema = Yup.object().shape(
+    {
+        valija : Yup.string().required('Valija requerida'),
+        bolita1 : Yup.number().required('Campo requerido'),
+        bolita2 : Yup.number().required('Campo requerido'),
+        bolita3 : Yup.number().required('Campo requerido'),
+        bolita4 : Yup.number().required('Campo requerido'),
+        bolita5 : Yup.number().required('Campo requerido'),
+        bolita6 : Yup.number().required('Campo requerido'),
+        bolita7 : Yup.number().required('Campo requerido'),
+        bolita8 : Yup.number().required('Campo requerido'),
+        bolita9 : Yup.number().required('Campo requerido')
+    }
+);
 
 
 const Valija3Monazos = () => {
     return (
-        <form>
-            <table className='table table-bordered'>
-                <thead>
-                    <tr>
-                        <th colSpan={11}>Pruebas realizadas antes del sorte<br/> Números favorecidos</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>Valija</th>
-                        <td><input type={'text'}/></td>
-                        <td><input type={'text'}/></td>
-                        <td><input type={'text'}/></td>
-                        <td><input type={'text'}/></td>
-                        <td><input type={'text'}/></td>
-                        <td><input type={'text'}/></td>
-                        <td><input type={'text'}/></td>
-                        <td><input type={'text'}/></td>
-                        <td><input type={'text'}/></td>
-                        <td><input type={'text'}/></td>
-                    </tr>
-                </tbody>
-            </table>
-        </form>
+        <div className='container'>
+            <Formik
+                initialValues={{}}
+                validationSchema={pruebasSchema}
+                onSubmit={()=>{}}
+                >
+                {({ values,
+                    touched,
+                    errors,
+                    isSubmitting,
+                    handleChange,
+                    handleBlur }) => (
+                        <Form>
+                            <table className='table table-bordered align-middle'>
+                                <thead>
+                                    <tr>
+                                        <th colSpan={11}>Pruebas realizadas antes del sorte<br/> Números favorecidos</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th>Valija <br/><Field id='valija' name='valija' type='text' className='form-control'/>
+                                                {
+                                                    errors.valija && touched.valija && 
+                                                    (
+                                                        <div>
+                                                            <ErrorMessage name='valija'></ErrorMessage>
+                                                        </div>
+                                                    )
+                                                }
+                                        </th>
+                                        <td><Field id='bolita1' name='bolita1' type='number' className='form-control'/>
+                                                {
+                                                    errors.bolita1 && touched.bolita1 && 
+                                                    (
+                                                        <div>
+                                                            <ErrorMessage name='bolita1'></ErrorMessage>
+                                                        </div>
+                                                    )
+                                                }
+                                        </td>
+                                        <td><Field id='bolita2' name='bolita2' type='number' className='form-control'/>
+                                                {
+                                                    errors.bolita2 && touched.bolita2 && 
+                                                    (
+                                                        <div>
+                                                            <ErrorMessage name='bolita2'></ErrorMessage>
+                                                        </div>
+                                                    )
+                                                }
+                                        </td>
+                                        <td><Field id='bolita3' name='bolita3' type='number' className='form-control'/>
+                                                {
+                                                    errors.bolita3 && touched.bolita3 && 
+                                                    (
+                                                        <div>
+                                                            <ErrorMessage name='bolita3'></ErrorMessage>
+                                                        </div>
+                                                    )
+                                                }
+                                        </td>
+                                        <td><Field id='bolita4' name='bolita4' type='number' className='form-control'/>
+                                                {
+                                                    errors.bolita4 && touched.bolita4 && 
+                                                    (
+                                                        <div>
+                                                            <ErrorMessage name='bolita4'></ErrorMessage>
+                                                        </div>
+                                                    )
+                                                }
+                                        </td>
+                                        <td><Field id='bolita5' name='bolita5' type='number' className='form-control'/>
+                                                {
+                                                    errors.bolita5 && touched.bolita5 && 
+                                                    (
+                                                        <div>
+                                                            <ErrorMessage name='bolita5'></ErrorMessage>
+                                                        </div>
+                                                    )
+                                                }
+                                        </td>
+                                        <td><Field id='bolita6' name='bolita6' type='number' className='form-control'/>
+                                                {
+                                                    errors.bolita6 && touched.bolita6 && 
+                                                    (
+                                                        <div>
+                                                            <ErrorMessage name='bolita6'></ErrorMessage>
+                                                        </div>
+                                                    )
+                                                }
+                                        </td>
+                                        <td><Field id='bolita7' name='bolita7' type='number' className='form-control'/>
+                                                {
+                                                    errors.bolita7 && touched.bolita7 && 
+                                                    (
+                                                        <div>
+                                                            <ErrorMessage name='bolita7'></ErrorMessage>
+                                                        </div>
+                                                    )
+                                                }
+                                        </td>
+                                        <td><Field id='bolita8' name='bolita8' type='number' className='form-control'/>
+                                                {
+                                                    errors.bolita8 && touched.bolita8 && 
+                                                    (
+                                                        <div>
+                                                            <ErrorMessage name='bolita8'></ErrorMessage>
+                                                        </div>
+                                                    )
+                                                }
+                                        </td>
+                                        <td><Field id='bolita9' name='bolita9' type='number' className='form-control'/>
+                                                {
+                                                    errors.bolita9 && touched.bolita9 && 
+                                                    (
+                                                        <div>
+                                                            <ErrorMessage name='bolita9'></ErrorMessage>
+                                                        </div>
+                                                    )
+                                                }
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </Form>
+                    )}
+            </Formik>
+        </div>
     );
 };
 
--- FrontEnd/saprofa_frontend/src/components/pure/forms/ValijaNuevosTiempos.jsx
+++ FrontEnd/saprofa_frontend/src/components/pure/forms/ValijaNuevosTiempos.jsx
@@ -1,77 +1,110 @@
 import React from 'react';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
-import { Card, CardContent, Grid } from '@mui/material';
 
-const pruebasSchema = Yup.object().shape(
-    {
-        valija : Yup.string()
-                .required('Valija requerida'),
-        
-    }
-);
+    const pruebasSchema = Yup.object().shape(
+        {
+            valija : Yup.string().required('Valija requerida'),
+            bolita1 : Yup.number().required('Campo requerido'),
+            bolita2 : Yup.number().required('Campo requerido'),
+            bolita3 : Yup.number().required('Campo requerido'),
+            bolita4 : Yup.number().required('Campo requerido'),
+            bolita5 : Yup.number().required('Campo requerido')
+        }
+    );
 
 const ValijaNuevosTiempos = () => {
-    const initialValues = {
-        valija : '',
-        bolitas : []
-    }
     return (
-        // <div>
-        //     <Formik>
-        //         {/* onSubmit={async (values) => {
-        //             await new Promise((r) => setTimeout(r,1000));
-        //             alert(JSON.stringify(values,null,2));
-        //             // We save the data in the localStorage
-        //             await localStorage.setItem('credentials', values);
-        //         }} */}
-        //         <table className='table table-bordered'>
-        //             <thead>
-        //                 <tr>
-        //                     <th colSpan={6}>Pruebas de bolitas Nuevos Tiempos y Reventados</th>
-        //                 </tr>
-        //             </thead>
-        //             <tbody>
-        //                 <tr>
-        //                     <th style={{width:'50px', height:'80px'}}>Valija <br/><input type={'text'}/></th>
-        //                     <td style={{width:'50px', height:'80px'}}><input type={'text'}/></td>
-        //                     <td style={{width:'50px', height:'80px'}}><input type={'text'}/></td>
-        //                     <td style={{width:'50px', height:'80px'}}><input type={'text'}/></td>
-        //                     <td style={{width:'50px', height:'80px'}}><input type={'text'}/></td>
-        //                     <td style={{width:'50px', height:'80px'}}><input type={'text'}/></td>
-        //                 </tr>
-        //             </tbody>
-        //         </table>
-        //     </Formik>
-        // </div>
-                <Formik 
-                    initialValues={{initialValues}}
-                    validationSchema={pruebasSchema}
-                    onSubmit={()=>{}}
-                >
-                    {({ values, 
-                        touched,
-                        errors,
-                        isSubmitting,
-                        handleChange,
-                        handleBlur
-                    }) =>(
-                        <Form>
-                            <label htmlFor="valija">Valija</label>
-                            <Field id="valija" type="text" name="valija" placeholder="Valija" size="3"/>
-                            <Field id="bolitas[0]" type="number"/>
-                            <Field id="bolitas[1]" type="number"/>
-                            <Field id="bolitas[2]" type="number"/>
-                            <Field id="bolitas[3]" type="number"/>
-                            <Field id="bolitas[4]" type="number"/>
-                            <Field id="bolitas[5]" type="number"/>
-                            <Field id="bolitas[6]" type="number"/>
-                            <Field id="bolitas[7]" type="number"/>
-                            <Field id="bolitas[8]" type="number"/>
-                            {/* <pre>{JSON.stringify({ values, errors }, null, 4)}</pre> */}
-                        </Form>
-                    )}
-                </Formik>
+        <div className='container'>
+            <Formik 
+                initialValues={{}}
+                validationSchema={pruebasSchema}
+                onSubmit={()=>{}}
+            >
+                {({ values, 
+                    touched,
+                    errors,
+                    isSubmitting,
+                    handleChange,
+                    handleBlur
+                }) =>(
+                    <Form>
+                        <table className='table table-bordered align-middle'>
+                            <thead>
+                                <tr>
+                                    <th colSpan={6}>Pruebas de bolitas Nuevos Tiempos y Reventados</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>Valija <br/><Field id='valija' name='valija' type='text' className='form-control'/>
+                                            {
+                                                errors.valija && touched.valija && 
+                                                (
+                                                    <div>
+                                                        <ErrorMessage name='valija'></ErrorMessage>
+                                                    </div>
+                                                )
+                                            }
+                                    </th>
+                                    <td><Field id='bolita1' name='bolita1' type='number' className='form-control'/>
+                                            {
+                                                errors.bolita1 && touched.bolita1 && 
+                                                (
+                                                    <div>
+                                                        <ErrorMessage name='bolita1'></ErrorMessage>
+                                                    </div>
+                                                )
+                                            }
+                                    </td>
+                                    <td><Field id='bolita2' name='bolita2' type='number' className='form-control'/>
+                                            {
+                                                errors.bolita2 && touched.bolita2 && 
+                                                (
+                                                    <div>
+                                                        <ErrorMessage name='bolita2'></ErrorMessage>
+                                                    </div>
+                                                )
+                                            }
+                                    </td>
+                                    <td><Field id='bolita3' name='bolita3' type='number' className='form-control'/>
+                                            {
+                                                errors.bolita3 && touched.bolita3 && 
+                                                (
+                                                    <div>
+                                                        <ErrorMessage name='bolita3'></ErrorMessage>
+                                                    </div>
+                                                )
+                                            }
+                                    </td>
+                                    <td><Field id='bolita4' name='bolita4' type='number' className='form-control'/>
+                                            {
+                                                errors.bolita4 && touched.bolita4 && 
+                                                (
+                                                    <div>
+                                                        <ErrorMessage name='bolita4'></ErrorMessage>
+                                                    </div>
+                                                )
+                                            }
+                                    </td>
+                                    <td><Field id='bolita5' name='bolita5' type='number' className='form-control'/>
+                                            {
+                                                errors.bolita5 && touched.bolita5 && 
+                                                (
+                                                    <div>
+                                                        <ErrorMessage name='bolita5'></ErrorMessage>
+                                                    </div>
+                                                )
+                                            }
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        {/* <pre>{JSON.stringify({ values, errors }, null, 4)}</pre> */}
+                    </Form>
+                )}
+            </Formik>
+        </div>
     );
 };
 
