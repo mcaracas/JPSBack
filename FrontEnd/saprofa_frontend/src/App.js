c6928ce54e887e4db816a74feb50a1346128f349
treicyleonzuniga <treicy.leon.zuniga@est.una.ac.cr>
2022-10-12 15:19:21 -0600

    IMPORT COMENTADOS

--- FrontEnd/saprofa_frontend/src/App.js
+++ FrontEnd/saprofa_frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import LotteryCardList from './components/container/LotteryCardList';
-import Marchamo3Monazos from './components/pure/forms/Marchamo3Monazos';
-import MarchamoLotto from './components/pure/forms/MarchamoLotto';
-import MarchamoNacional from './components/pure/forms/MarchamoNacional';
-import MarchamoNuevosTiempos from './components/pure/forms/MarchamoNuevosTiempos';
-import MarchamoPopular from './components/pure/forms/MarchamoPopular';
-import LoginPage from './pages/auth/LoginPage';
+//import LotteryCardList from './components/container/LotteryCardList';
+//import Marchamo3Monazos from './components/pure/forms/Marchamo3Monazos';
+//import MarchamoLotto from './components/pure/forms/MarchamoLotto';
+//import MarchamoNacional from './components/pure/forms/MarchamoNacional';
+//import MarchamoNuevosTiempos from './components/pure/forms/MarchamoNuevosTiempos';
+//import MarchamoPopular from './components/pure/forms/MarchamoPopular';
+//import LoginPage from './pages/auth/LoginPage';
+//import ListItem from './components/pure/List-item';
 
 function App() {
   return (
@@ -13,11 +14,15 @@ function App() {
         {/** @TODO: Routes */}
         {/* <LoginPage></LoginPage> * Login page */}
         {/* <LotteryCardList></LotteryCardList> */}
-        <Marchamo3Monazos></Marchamo3Monazos>
-        <MarchamoLotto></MarchamoLotto>
-        <MarchamoNacional></MarchamoNacional>
-        <MarchamoNuevosTiempos></MarchamoNuevosTiempos>
-        <MarchamoPopular></MarchamoPopular>
+        {/*<Marchamo3Monazos></Marchamo3Monazos>*/}
+        {/*<MarchamoLotto></MarchamoLotto>*/}
+        {/*<MarchamoNacional></MarchamoNacional>*/}
+        {/*<MarchamoNuevosTiempos></MarchamoNuevosTiempos>*/}
+        {/*<MarchamoPopular></MarchamoPopular>*/}
+        
+
+
+
 
     </div>
   );
