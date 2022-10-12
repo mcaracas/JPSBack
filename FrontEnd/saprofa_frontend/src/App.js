1da19938e853e65d85d5fbf0d4c7e0f8f17636b8
treicyleonzuniga <treicy.leon.zuniga@est.una.ac.cr>
2022-10-12 17:10:27 -0600

    APP

--- FrontEnd/saprofa_frontend/src/App.js
+++ FrontEnd/saprofa_frontend/src/App.js
@@ -1,33 +1,28 @@
 import './App.css';
-//import LotteryCardList from './components/container/LotteryCardList';
-//import Marchamo3Monazos from './components/pure/forms/Marchamo3Monazos';
-//import MarchamoLotto from './components/pure/forms/MarchamoLotto';
-//import MarchamoNacional from './components/pure/forms/MarchamoNacional';
-//import MarchamoNuevosTiempos from './components/pure/forms/MarchamoNuevosTiempos';
-//import MarchamoPopular from './components/pure/forms/MarchamoPopular';
+import LotteryCardList from './components/container/LotteryCardList';
+import Marchamo3Monazos from './components/pure/forms/Marchamo3Monazos';
+import MarchamoLotto from './components/pure/forms/MarchamoLotto';
+import MarchamoNacional from './components/pure/forms/MarchamoNacional';
+import MarchamoNuevosTiempos from './components/pure/forms/MarchamoNuevosTiempos';
+import MarchamoPopular from './components/pure/forms/MarchamoPopular';
 import LoginPage from './pages/auth/LoginPage';
-
-import NombreFiscalizadores from './pages/auth/NombreFiscalizadores';
 
 function App() {
   return (
     <div className="App">
         {/** @TODO: Routes */}
-        {/* <LoginPage></LoginPage> * Login page */}
+        <LoginPage></LoginPage>
         {/* <LotteryCardList></LotteryCardList> */}
-        {/*<Marchamo3Monazos></Marchamo3Monazos>*/}
-        {/*<MarchamoLotto></MarchamoLotto>*/}
-        {/*<MarchamoNacional></MarchamoNacional>*/}
-        {/*<MarchamoNuevosTiempos></MarchamoNuevosTiempos>*/}
-        {/*<MarchamoPopular></MarchamoPopular>*/}
-        <NombreFiscalizadores></NombreFiscalizadores>
-        
-  
-        
-
-
-
-
+        {/* <h1>3 Monazos</h1>
+        <Marchamo3Monazos></Marchamo3Monazos>
+        <h1>Nuevos Tiempos</h1>
+        <MarchamoNuevosTiempos></MarchamoNuevosTiempos>
+        <h1>Lotto</h1>
+        <MarchamoLotto></MarchamoLotto>
+        <h1>Popular</h1>
+        <MarchamoPopular></MarchamoPopular>
+        <h1>Nacional</h1>
+        <MarchamoNacional></MarchamoNacional> */}
     </div>
   );
 }
