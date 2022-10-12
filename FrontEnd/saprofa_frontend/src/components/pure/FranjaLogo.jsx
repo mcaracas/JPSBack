5e38ea25319aafdeedec9f2accc43ba517bd637a
treicyleonzuniga <treicy.leon.zuniga@est.una.ac.cr>
2022-10-12 16:14:04 -0600

    FRANJA BLANCA

---
+++ FrontEnd/saprofa_frontend/src/components/pure/FranjaLogo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './franjaLogo.scss'
+
+
+
+function FranjaLogo(){
+return (<header className="franjaLogo">
+
+
+<img src={require ("../../img/logo_jps.png")} alt="LOGO"/>
+</header>
+)
+
+
+
+}
+
+export default FranjaLogo
