f3e46b573d5983f38c935feaad256b7ad90868e5
Jorge Carranza <73572833+JorgeCQ07@users.noreply.github.com>
2022-10-15 15:22:27 -0600

    Fixed col positions

--- FrontEnd/saprofa_frontend/src/components/pure/List.jsx
+++ FrontEnd/saprofa_frontend/src/components/pure/List.jsx
@@ -1,31 +1,30 @@
 import React, { useDebugValue } from 'react'
 import ListItem from './List-item'
+import './../container/contenedorLista.scss'
 
 const List = ({ items, titulo, juez }) => {
   // let nombrejuez = items[0].juez;
   return (
     <div>
-      <div className='diplay-flex justify-content-between'>
-        <div  className='row'>
-          <h2 className='col'>{titulo}</h2>
-          <h2 className='col'>Juez</h2>
-        </div>
-        <div className='row'>
-          <div className='col'>
+      <div className='row justify-content-between'>
+        <div className='col-6 mt-2'>
+          <h3 className='text-center'>{titulo}</h3>
+          <div className='row'>
             <ul>
               {items.map(item => (
-                <ListItem key={item.id} item={item.nombre}/>
+                <ListItem key={item.id} item={item.nombre} />
               ))}
             </ul>
           </div>
-          <div className='col'>
-                <li>{juez}</li>
+        </div>
+        <div className='col-5 mt-3'>
+          <h2 className='text-center'>Juez</h2>
+          <div className='row'>
+            <li>{juez}</li>
           </div>
         </div>
       </div>
     </div>
-
-
   )
 }
 
