454c5776a22db2aa71aecd8ed33b8b4a4fb81fe8
Jorge Carranza <73572833+JorgeCQ07@users.noreply.github.com>
2022-10-14 23:11:28 -0600

    Get login time from server

--- FrontEnd/saprofa_frontend/src/services/axiosService.js
+++ FrontEnd/saprofa_frontend/src/services/axiosService.js
@@ -1,17 +1,17 @@
 import APIRequest from "../utils/config/axios.config";
 
 /**
- * @param {String} username 
- * @param {String} password 
+ * @param {Object} values
  * @returns post request
  */
-export function login(values) {
-    const data ={
-        '"id"': values.username,
-        '"clave"': values.password
-    }
-    console.log(data);
+ export function login(values) {    // Check credentials
+    const data ={"id": values.username, 
+    "clave": values.password.toString()}
     return APIRequest.post('/usuario/login',data);
+}
+
+export function getHoraIngreso(){   // Get the current date and time from the server
+    return APIRequest.get('/util/FechaHoraActual');
 }
 
 export function insertMarchamo(marchamoList){
