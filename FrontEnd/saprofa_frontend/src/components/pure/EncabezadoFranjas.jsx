3adf0d894795ac53952179137c64266ee5fc135e
cabez <cabezasvizcaino@gmail.com>
2022-10-31 11:12:23 -0600

    Create register and fix login

--- FrontEnd/saprofa_frontend/src/components/pure/EncabezadoFranjas.jsx
+++ FrontEnd/saprofa_frontend/src/components/pure/EncabezadoFranjas.jsx
@@ -4,14 +4,14 @@ import FranjaRoja from './header/FranjaR'
 import React from 'react';
 import PropTypes from 'prop-types'
 
-const EncabezadoFranjas = ({title}) => {
-    return (
-        <div>
-      <FranjaLogo/>
-      <FranjaPrincipal/>
-      <FranjaRoja title={title}/>
-        </div>
-    );
+const EncabezadoFranjas = ({ title }) => {
+  return (
+    <div>
+      <FranjaLogo />
+      <FranjaPrincipal />
+      <FranjaRoja title={title} />
+    </div>
+  );
 }
 
 FranjaRoja.propTypes = {
