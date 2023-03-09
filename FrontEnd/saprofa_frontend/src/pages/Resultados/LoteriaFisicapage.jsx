f4ca2f1ad4a7d2e43474ea3245c0be8e971952dd
Gianluca Gibellato <gnlgibe@gmail.com>
2023-03-08 22:27:24 -0600

    Agrega props de num_sorteo y tipo_loteria

--- FrontEnd/saprofa_frontend/src/pages/Resultados/LoteriaFisicapage.jsx
+++ FrontEnd/saprofa_frontend/src/pages/Resultados/LoteriaFisicapage.jsx
@@ -3,10 +3,13 @@ import EncabezadoFranjas from '../../components/pure/EncabezadoFranjas';
 import ResultadoLoteriaFisica from '../../components/pure/Resultados/ResultadoLoteriaFisica';
 
 const LoteriaFisicapage = () => {
+    const num_sorteo = sessionStorage.getItem('num_sorteo');
+    const tipo_loteria = sessionStorage.getItem('tipo_loteria');
+    // TODO: change num_sorteo and tipo_loteria to the values from the session storage
     return (
         <div>
             <EncabezadoFranjas title={"Resultados Loteria Fisica"}/>
-            <ResultadoLoteriaFisica/>
+            <ResultadoLoteriaFisica num_sorteo='3360' tipo_loteria='LN'/>
         </div>
     );
 }
