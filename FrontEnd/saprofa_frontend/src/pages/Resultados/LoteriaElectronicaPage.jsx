8122012e4c0f5d24d4d9b18459871e3643ce3b6c
Jorge Carranza <73572833+JorgeCQ07@users.noreply.github.com>
2023-03-08 22:39:49 -0600

    Created resultados loteria electronica page

---
+++ FrontEnd/saprofa_frontend/src/pages/Resultados/LoteriaElectronicaPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import EncabezadoFranjas from '../../components/pure/EncabezadoFranjas';
+import ResultadosLotto from '../../components/pure/Resultados/ResultadosLotto';
+import Resultados3M from '../../components/pure/Resultados/Resultados3M';
+import ResultadosNT from '../../components/pure/Resultados/ResultadosNT';
+
+
+const LoteriaElectronicaPage = () => {
+
+    // @TODO: Get the type of lottery from the session storage
+   //const tipoSorteo = sessionStorage.getItem("")
+
+   let tipoSorteo = "LTT";
+   
+    return (
+        <div>
+            <EncabezadoFranjas title={"Resultados loteria electronica"}></EncabezadoFranjas>
+            {tipoSorteo === "LTT" ? <ResultadosLotto/> : 
+             tipoSorteo === "3M" ? <Resultados3M/> :
+             tipoSorteo === "NT" ? <ResultadosNT/> : null}
+        </div>
+    );
+}
+
+export default LoteriaElectronicaPage;
