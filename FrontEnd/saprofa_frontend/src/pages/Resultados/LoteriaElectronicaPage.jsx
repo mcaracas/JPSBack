26e2b3200499e58f3038f5dfec091a7e5c8647aa
Jorge Carranza <73572833+JorgeCQ07@users.noreply.github.com>
2023-03-15 14:01:54 -0600

    Fixed page title

--- FrontEnd/saprofa_frontend/src/pages/Resultados/LoteriaElectronicaPage.jsx
+++ FrontEnd/saprofa_frontend/src/pages/Resultados/LoteriaElectronicaPage.jsx
@@ -8,16 +8,16 @@ import ResultadosNT from '../../components/pure/Resultados/ResultadosNT';
 const LoteriaElectronicaPage = () => {
 
     // @TODO: Get the type of lottery from the session storage
-   //const tipoSorteo = sessionStorage.getItem("")
+    //const tipoSorteo = sessionStorage.getItem("")
 
-   let tipoSorteo = "LTT";
-   
+    let tipoSorteo = "LTT";
+
     return (
         <div>
-            <EncabezadoFranjas title={"Resultados loteria electronica"}></EncabezadoFranjas>
-            {tipoSorteo === "LTT" ? <ResultadosLotto/> : 
-             tipoSorteo === "3M" ? <Resultados3M/> :
-             tipoSorteo === "NT" ? <ResultadosNT/> : null}
+            <EncabezadoFranjas title={"Resultados lotería electrónica"}></EncabezadoFranjas>
+            {tipoSorteo === "LTT" ? <ResultadosLotto /> :
+                tipoSorteo === "3M" ? <Resultados3M /> :
+                    tipoSorteo === "NT" ? <ResultadosNT /> : null}
         </div>
     );
 }
