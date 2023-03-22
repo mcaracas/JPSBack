4172a4bf8a0bb3764bb00f4280d893b274e3a057
Treicy Leon <treicy.leon.zuniga@est.una.ac.cr>
2023-03-21 23:27:50 -0600

    axios

--- FrontEnd/saprofa_frontend/src/services/axiosService.js
+++ FrontEnd/saprofa_frontend/src/services/axiosService.js
@@ -139,10 +139,4 @@ export function postTomoFolio(data){
     return APIRequest.post('/TomoFolio',data);
 }
 
-export function getUltimoTomofolio(idInterno){
-    return APIRequest.get('/TomoFolio/UltimoTomofolio/'+idInterno);
-}
 
-export function postTomoFolio(data){
-    return APIRequest.post('/TomoFolio',data);
-}
