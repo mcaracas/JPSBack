52dd8b199afeb0a2fe02a81f1b99489083468419
Gianluca Gibellato <gnlgibe@gmail.com>
2023-04-19 23:48:59 -0600

    Fix Datos Participantes
    Falta Mapear correctamente los datos que se envian

--- FrontEnd/saprofa_frontend/src/components/pure/datosParticipantes/DatosParticipantes.jsx
+++ FrontEnd/saprofa_frontend/src/components/pure/datosParticipantes/DatosParticipantes.jsx
@@ -1,11 +1,15 @@
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import { useState, useRef } from 'react';
-import { getDatosParticipantes, insertDatosAdministracion } from '../../../services/axiosService';
+import { Formik, Form } from 'formik';
+import { useState, useEffect } from 'react';
+import { insertDatosAdministracion } from '../../../services/axiosService';
 import SuccessModal from '../../modals/SuccessModal';
+import ParticipantesDropdown from './ParticipantesDropdown';
+import LoadingModal from "../../modals/LoadingModal";
+import FailModal from "../../modals/FailModal";
+import ConfirmationModal from "../../modals/ConfirmationModal";
+import { useNavigate } from 'react-router-dom';
 
 
 const initialValues = {
-  id : '',
   GOperaciones: '',
   GProduccionYComercializacion: '',
   Gerencia: '',
@@ -15,175 +19,203 @@ const initialValues = {
   EquipoDeComputo: ''
 };
 
+//const nombresParticipantes = [ 'Gerente Operaciones', 'Gerente Producción', 'Gerencia', 'Juez', 'Presentador', 'Prompter', 'Equipo Computo' ];
+
 // check the props. should include num_sorteo, tipo_loteria
-const DatosParticipantes = ({ idSorteo }) => {
+const DatosParticipantes = ({ idSorteo, objetoDatosMapeados, obtenerDatosAdministracion }) => {
   const [showSuccessModal, setShowSuccessModal] = useState(false);
-  const [datosEnviados, setDatosEnviados] = useState(false);
-  const [formData, setFormData] = useState(initialValues);
+  const [showLoadingModal, setShowLoadingModal] = useState(false);
+  const [showFailModal, setShowFailModal] = useState(false);
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const navigate = useNavigate();
   const [titulo, setTitulo] = useState('');
   const [mensaje, setMensaje] = useState('');
-  const formRef = useRef(null);
+  const [datosEnviados, setDatosEnviados] = useState(false);
+  const [formData, setFormData] = useState(initialValues);
+  const [labelsParticipantes, setLabelsParticipantes] = useState(Object.keys(objetoDatosMapeados));
+  const [datosParticipantes, setDatosParticipantes] = useState(Object.values(objetoDatosMapeados));
+  const [dropdownValues, setDropdownValues] = useState([]);
+  const [confirmationAction, setConfirmationAction] = useState(() => { });
 
   function handleCloseSuccessModal() {
     setShowSuccessModal(false);
+    //TODO: check where to navigate
+    navigate('/PruebasLotto');
   }
 
-  const obtenerDatosAdministracion = async (idSorteo) => {
-    try{
-      // TODO: wait for the endpoint to receive the idSorteo, 
-      // TODO: meanwhile is receiving all data, it has to retrieve data from administracion
-      const response = await getDatosParticipantes(1);
-      // el BE devuelve un array con todos los datos porque no se esta pidiendo un id especifico
-      const datos = response.data[6];
-      const datosMapeados = {
-        // id: datos.id_datos_previos, has to be added by session storage
-        GOperaciones: datos.gerenteOperaciones,
-        GProduccionYComercializacion: datos.gerenteProduccion,
-        Gerencia: datos.gerencia,
-        Juez: datos.juez,
-        PresentadorDelSorteo: datos.presentador,
-        Prompter: datos.prompter,
-        EquipoDeComputo: datos.equipoComputo ,
-      };
-      console.log("datosMapeados: ",datosMapeados);
-      setFormData({...datosMapeados});
-      if (datos) {
-          const formValues = { ...datosMapeados };
-          formRef.current.setValues(formValues);
-      }
-    } catch (error) {
-      console.log(error);
+  function handleCloseFailModal() {
+    setShowFailModal(false);
+  }
+
+  const handleConfirmation = async (confirmed) => {
+    if (!confirmed) {
+      setShowConfirmation(false);
+      return;
     }
-  };
+    await confirmationAction();
+    setShowConfirmation(false);
+  }
+
+  const handleShowConfirmation = async (action) => {
+    setShowConfirmation(true);
+    setConfirmationAction(() => () => {
+      action();
+    });
+  }
+
+  useEffect(() => {
+    handleObtenerParticipantes();
+    labelsParticipantes.forEach(label => {
+      const llave = mapLabel(label);
+      const val = objetoDatosMapeados[label];
+      console.log("key", llave);
+      console.log("val", val);
+      setFormData({ ...formData, [llave]: val });
+    });
+    console.log("formData", formData);
+  }, []);
+
+  const mapLabel = label => {
+    switch (label) {
+      case 'Gerente Operaciones':
+        return 'GOperaciones';
+      case 'Gerencia Produccion y Comercializacion':
+        return 'GProduccionYComercializacion';
+      case 'Gerencia':
+        return 'Gerencia';
+      case 'Juez':
+        return 'Juez';
+      case 'Presentador del Sorteo':
+        return 'PresentadorDelSorteo';
+      case 'Prompter':
+        return 'Prompter';
+      case 'Equipo de Computo':
+        return 'EquipoDeComputo';
+      default:
+        return '';
+    }
+  }
+
+  const setParticipanteForm = (label, value) => {
+    const labelMapped = mapLabel(label);
+    setFormData({ ...formData, [labelMapped]: value });
+  }
+
+  const handleObtenerParticipantes = async () => {
+    try {
+      if (!idSorteo) {
+        const lottery = JSON.parse(sessionStorage.getItem('lottery'));
+        idSorteo = lottery?.idInterno;
+      }
+      const response = await obtenerDatosAdministracion(idSorteo);
+      const labels = Object.keys(response);
+      const datos = Object.values(response);
+      setLabelsParticipantes(labels);
+      setDatosParticipantes(datos);
+      const dropdowns = labelsParticipantes.map((label, index) => {
+        return (
+          <div className="col-12 col-md-6 m-4" key={index}>
+            <label htmlFor={label}>{label}</label>
+            {console.log("objetoDatosMapeados[label]", objetoDatosMapeados[label])}
+            {/* <ParticipantesDropdown participantes={datosParticipantes} setFieldValue={setFieldValue} /> */}
+            <ParticipantesDropdown participantes={datosParticipantes} value={objetoDatosMapeados[label]} label={label} setParticipanteForm={setParticipanteForm} />
+          </div>
+        )
+      });
+      setDropdownValues(dropdowns);
+
+      console.log("datosParticipantes", datosParticipantes);
+    } catch (error) {
+
+    }
+  }
+
+  const handleSubmit = async (values) => {
+    console.log("values", values);
+    try {
+      // setShowLoadingModal(true);
+      // const response = await insertDatosAdministracion({
+      //   gerenteOperaciones: values.GOperaciones,
+      //   gerenteProduccion: values.GProduccionYComercializacion,
+      //   gerencia: values.Gerencia,
+      //   juez: values.Juez,
+      //   presentadorDelSorteo: values.PresentadorDelSorteo,
+      //   prompter: values.Prompter,
+      //   equipoDeComputo: values.EquipoDeComputo
+      // });
+      // setShowLoadingModal(false);
+      // if (response.status === 200) {
+      //   setShowLoadingModal(false);
+      //   setMensaje("Datos de Participantes guardados exitosamente");
+      //   setTitulo("¡Operación Exitosa!");
+      //   setDatosEnviados(true);
+      //   setShowSuccessModal(true);
+      // } else {
+      //   setShowLoadingModal(false);
+      // }
+    } catch (error) {
+      setShowLoadingModal(false);
+      setMensaje(`Error al guardar los Datos de Participantes. ${error.message}`);
+      setTitulo("¡Operación Fallida!");
+      setDatosEnviados(false);
+      setShowFailModal(true);
+    }
+  }
 
   return (
     <>
-    <div className="container">
-      <Formik
-        initialValues={formData}
-        validate={values => {
-          const errors = {};
-          if (!values.GOperaciones) {
-            errors.GOperaciones = 'Este campo es requerido';
-          }
-          if (!values.GProduccionYComercializacion) {
-            errors.GProduccionYComercializacion = 'Este campo es requerido';
-          }
-          if (!values.Gerencia) {
-            errors.Gerencia = 'Este campo es requerido';
-          }
-          if (!values.Juez) {
-            errors.Juez = 'Este campo es requerido';
-          }
-          if (!values.PresentadorDelSorteo) {
-            errors.PresentadorDelSorteo = 'Este campo es requerido';
-          }
-          if (!values.Prompter) {
-            errors.Prompter = 'Este campo es requerido';
-          }
-          if (!values.EquipoDeComputo) {
-            errors.EquipoDeComputo = 'Este campo es requerido';
-          }
-
-          return errors;
-        }}
-        onSubmit={ async (values, { setSubmitting, handleFormSubmit }) => {
-          try{
-            const response = await insertDatosAdministracion({
-              gerenteOperaciones : values.GOperaciones,
-              gerenteProduccion :  values.GProduccionYComercializacion,
-              gerencia : values.Gerencia,
-              juez : values.Juez,
-              presentador : values.PresentadorDelSorteo,
-              prompter : values.Prompter,
-              equipoComputo : values.EquipoDeComputo,
-            });
-            if(response.status === 200){
-              setSubmitting(false);
-              setDatosEnviados(true);
-              setMensaje("Datos de Participantes guardados exitosamente");
-              setTitulo("¡Operación Exitosa!");
-              setShowSuccessModal(true);
-              console.log("Datos de administracion guardados",values);
+      <div className="container">
+        <Formik
+          initialValues={formData}
+          onSubmit={
+            async () => {
+              await handleShowConfirmation(() => handleSubmit(formData));
             }
-          } catch (error) {
-            console.log(error);
           }
-        }}
-        innerRef={formRef}
-      >
-        {({ errors, isSubmitting, isValidating }) => (
-          <div className='container-fluid col'>
-            <Form>
-              <div className="row">
-                <div className="col-8">
-                  <div className="col-12 col-md-6 m-4">
-                    <h5 className='text-start'>Por G. Operaciones</h5>
-                    <Field className="form-control" name="GOperaciones" />
-                    <ErrorMessage name="GOperaciones" component={() => {
-                      return <div className='error'>{errors.GOperaciones}</div>
-                    }}/>
+        >
+          {({ isSubmitting, isValidating }) => (
+            <div className='container-fluid col'>
+              <Form>
+                <div className="row">
+                  <div className="col-8">
+                    {
+                      dropdownValues.map(dropdown => dropdown)
+                    }
                   </div>
-                  <div className="col-12 col-md-6 m-4">
-                    <h5 className='text-start'>Por G. Produccion y Comercializacion</h5>
-                    <Field className="form-control" name="GProduccionYComercializacion" />
-                    <ErrorMessage name="GProduccionYComercializacion" component={() => {
-                      return <div className='error'>{errors.GProduccionYComercializacion}</div>
-                    }}/> 
-                  </div>
-                  <div className="col-12 col-md-6 m-4">
-                    <h5 className='text-start'>Por Gerencia</h5>
-                    <Field className="form-control" name="Gerencia" />
-                    <ErrorMessage name="Gerencia" component={() => {
-                      return <div className='error'>{errors.Gerencia}</div>
-                    }}/>
-                  </div>
-                  <div className="col-12 col-md-6 m-4">
-                    <h5 className='text-start'>Por Juez</h5>
-                    <Field className="form-control" name="Juez" />
-                    <ErrorMessage name="Juez" component={() => {
-                      return <div className='error'>{errors.Juez}</div>
-                    }}/>
-                  </div>
-                  <div className="col-12 col-md-6 m-4">
-                    <h5 className='text-start'>Presentador del Sorteo</h5>
-                    <Field className="form-control" name="PresentadorDelSorteo" />
-                    <ErrorMessage name="PresentadorDelSorteo" component={() => {
-                      return <div className='error'>{errors.PresentadorDelSorteo}</div>
-                    }}/>
-                  </div>
-                  <div className="col-12 col-md-6 m-4">
-                    <h5 className='text-start'>Prompter</h5>
-                    <Field className="form-control" name="Prompter" />
-                    <ErrorMessage name="Prompter" component={() => {
-                      return <div className='error'>{errors.Prompter}</div>
-                    }}/>
-                  </div>
-                  <div className="col-12 col-md-6 m-4">
-                    <h5 className='text-start'>Equipo de Computo</h5>
-                    <Field className="form-control" name="EquipoDeComputo" />
-                    <ErrorMessage name="EquipoDeComputo" component={() => {
-                      return <div className='error'>{errors.EquipoDeComputo}</div>
-                    }}/>
+                  <div className='col-2'>
+                    <div className="col-12 col-md-6 d-flex flex-column justify-content-between">
+                      <button className="btn mt-5 mb-5" name='obtenerDatos' type="button" onClick={handleObtenerParticipantes}>Obtener Participantes</button>
+                      <button className="btn mt-5 mb-5" disabled={isSubmitting || isValidating || datosEnviados} type="submit" onClick={() => { insertDatosAdministracion() }}>Registar Datos de Participantes</button>
+                    </div>
                   </div>
                 </div>
-                <div className='col-2'>
-                  <div className="col-12 col-md-6 d-flex flex-column justify-content-between">
-                    <button className="btn mt-5 mb-5" name='obtenerDatos' type="button" onClick={()=>obtenerDatosAdministracion(idSorteo)}>Obtener Participantes</button>
-                    <button className="btn mt-5 mb-5" disabled={isSubmitting || isValidating || datosEnviados} type="submit" onClick={()=>{insertDatosAdministracion()}}>Registar Datos de Participantes</button>
-                  </div>
-                </div>
-              </div>
-            </Form>
-          </div>
-        )}
-      </Formik>
-    </div>
-    <SuccessModal 
-      show={showSuccessModal}
-      handleClose={handleCloseSuccessModal}
-      titulo = {titulo}
-      mensaje = {mensaje}
+              </Form>
+            </div>
+          )}
+        </Formik>
+      </div>
+      <SuccessModal
+        show={showSuccessModal}
+        titulo={titulo}
+        mensaje={mensaje}
+        handleClose={handleCloseSuccessModal}
+      />
+      <LoadingModal
+        show={showLoadingModal}
+        titulo='Guardando Datos de Participantes'
+        mensaje='Por favor espere...'
+      />
+      <FailModal
+        show={showFailModal}
+        titulo={titulo}
+        mensaje={mensaje}
+        handleClose={handleCloseFailModal}
+      />
+      <ConfirmationModal
+        show={showConfirmation}
+        titulo='Confirmación'
+        mensaje='¿Está seguro que desea registrar los datos de los participantes?'
+        handleConfirmation={handleConfirmation}
       />
     </>
   );
